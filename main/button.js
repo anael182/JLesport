e816737f12a33e6e1c591552cb7710067870893b
anael182 <anael.bouchema@gmail.com>
2022-01-10 00:12:54 +0100

    jquery

--- main/button.js
+++ main/button.js
@@ -1,11 +1,8 @@
 $(window).ready(function(){
     $(".boton").wrapInner('<div class=botontext></div>');
-        
-        $(".botontext").clone().appendTo( $(".boton") );
-        
-        $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
-        
-        $(".twist").css("width", "25%").css("width", "+=3px");
+    $(".botontext").clone().appendTo( $(".boton") );       
+    $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');       
+    $(".twist").css("width", "25%").css("width", "+=3px");
     });
-console.log("je suis chargé");
+console.log("loaded from button.js");
     
