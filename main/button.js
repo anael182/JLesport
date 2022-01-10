b44a7dd476b993dd360cc27b0a28bfe7e80c12e2
anael182 <anael.bouchema@gmail.com>
2022-01-10 13:01:17 +0100

    Update button.js

--- main/button.js
+++ main/button.js
@@ -1,13 +1,7 @@
 $(window).ready(function(){
     $(".boton").wrapInner('<div class=botontext></div>');
-    console.log("1st instruction passed");
-    $(".botontext").clone().appendTo( $(".boton") );
-    console.log("2nd instruction passed");       
-    $(".boton").append('<span class="twist"></span>'); 
-    $(".boton").append('<span class="twist"></span>'); 
-    $(".boton").append('<span class="twist"></span>'); 
-    $(".boton").append('<span class="twist"></span>'); 
-    console.log("3rd instruction passed");       
+    $(".botontext").clone().appendTo($(".boton"));
+    $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
     $(".twist").css("width", "25%").css("width", "+=3px");
-    console.log("4th instruction passed"); 
-    });    
+    }
+);   
